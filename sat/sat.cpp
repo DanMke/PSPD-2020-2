ba10763a3a0eaa4857e7adc72365029605cf2aea
Daniel Maike <danmke@hotmail.com>
2021-03-08 22:24:06 -0300

    Added first implementation of SAT (boolean satisfiability)
    Co-authored-by: Joberth Rogers <joberth.rogers18@gmail.com>

---
+++ sat/sat.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+// #include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+maximo de variaveis = 2**20
+maximo de clausulas = 2**27
+maximo de literais em uma clausula eh o maximo de variaveis da formula
+possibilidades = 2**n (n = numero de variaveis)
+minimo de 1 variavel
+*/
+
+int main() {
+    cout << "Hello World!" << endl << "SAT - Satisfabilidade Booleana" << endl;
+
+    // Quantas variaveis eu tenho?
+    // Quantas clausulas eu tenho? (o 0 representa o fim de uma clausula)
+    // os numerais representam os literais das variaveis, quando positivo representa o literal positivo e quando negativo o literal negado
+
+    // ex input: 2 (variaveis) 3 (clausulas)
+    // 1 2 0
+    // -1 2 0
+    // -1 -2 0
+    // valoracao ex: 1 2 (1 verdadeiro e 2 verdadeiro)
+
+    // output:
+    // quantas clausulas nao foram satisfeitas
+    // uma clausula eh satisfeita quando pelo menos um literal tiver valoracao verdadeira
+
+    int numberClauses = 0, numberVariables = 0, numberNotSatisfiedClauses = 0;
+    vector<vector<int>> clauses;
+    vector<int> valuation;
+    // v[0] = 1 2 -3
+
+    cout << "Number of variables:" << endl;
+    cin >> numberVariables;
+    cout << "Number of clauses:" << endl;
+    cin >> numberClauses;
+
+    cout << "Clauses:" << endl;
+
+    for (int i = 0; i < numberClauses; i++) {
+        vector<int> auxVector;
+        while (true) {
+            int temp;
+            cin >> temp;
+            if (temp == 0) {
+                break;
+            }
+            auxVector.push_back(temp);
+        }
+        clauses.push_back(auxVector);
+    }
+
+    for (vector<int> clause : clauses) {
+        cout << "clause:" << endl;
+        for (int variable : clause) {
+            cout << variable << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "Valuation:" << endl;
+    for (int i = 0; i < numberVariables; i++) {
+        int temp;
+        cin >> temp;
+        valuation.push_back(temp);
+    }
+
+    // percorrer o vetor de clausulas, pegando cada variavel de uma clausula e verificando com o valuation
+    int counter = 0;
+    for (vector<int> clause : clauses) {
+        int clauseSize = clause.size();
+        for (int variable : clause) {
+            // variavel 1 = valuation[0]
+            int variableValuation = abs(variable); // -1 >> 1
+            if (variable == valuation[variableValuation - 1]) { // valuation[1 - 1]
+                break;
+            } 
+            // se variable == -1 (negado) e valuation[1 - 1] == 1 (positivo) ... valuation[2 - 1] == -2 (negado)
+            clauseSize--;
+        }
+        if (clauseSize == 0) {
+            numberNotSatisfiedClauses++;
+            cout << "Clausula " << counter + 1 << " Não Satisfeita" << endl;
+        } else {
+            cout << "Clausula " << counter + 1 << " Satisfeita" << endl;
+        }
+        counter++;
+    }
+
+    cout << "Number of Not Satisfied Clauses: " << numberNotSatisfiedClauses << endl;
+
+    return 0;
+}
+
+
+// ler ate o 0 que eh o final da clausula
+// v[0] = 1 2 3
+// v[1] = 1 -2 -3
+// v[2] = -1 -2 -3
